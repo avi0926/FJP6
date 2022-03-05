11c6ca9e2e93ac665bde801fefbd27699af27a49
avi0926 <abhinandanmaurya230@gmail.com>
2022-03-05 22:42:21 +0530

    functions

---
+++ module2/function.js
@@ -0,0 +1,31 @@
+//function in js without parameter and without return type
+function sayHello(){
+    console.log("Hello from function");
+}
+
+sayHello();
+
+//function with parameter
+
+function sum(num1, num2){
+    let addition = num1+num2;
+    console.log("addition of given numbers: "+ addition);
+}
+
+sum(5,10);
+
+//unction with return type
+
+function mul(num1,num2){
+    return num1*num2;
+}
+
+let ans = mul(3,5);
+console.log('multiply of two number'+ ans);
+
+//storing function in a variable
+//functions are first class citizen in js -> (interview) because variable mai store ho skti hai
+let var1 = function sub(num1,num2){
+    return num1-num2;
+}
+console.log(var1(5,2));
