731cdfa94599e04a3f51aeadb0978760d615482b
avi0926 <abhinandanmaurya230@gmail.com>
2022-03-08 21:53:48 +0530

    objects

---
+++ module2/objects.js
@@ -0,0 +1,13 @@
+//empty obect
+let obj = {};
+console.log(obj);
+
+let objavi = {
+    "Name": "Avi",
+    "Age": '23',
+    "phone No": 7985727157,
+    // key by-default string hi rhegi
+    lastName: 'Maurya'
+};
+
+console.log(objavi);
