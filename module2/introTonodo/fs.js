4c19a38894ee8c3eae85d0f4ddd789838d52b692
avi0926 <abhinandanmaurya230@gmail.com>
2022-03-11 18:26:45 +0530

    path

--- module2/introTonodo/fs.js
+++ module2/introTonodo/fs.js
@@ -5,4 +5,20 @@ let path = require("path");
 let filePath = path.join(__dirname,"file.txt");
 console.log(filePath);
 
+
+// C R U D-----------------
+
+// C-creat
 fs.writeFileSync(filePath,"Again writing on existing file");
+
+// R-read
+let content = fs.readFileSync(filePath,'utf-8');
+console.log(content);
+
+// U-update
+fs.appendFileSync(filePath,"newly added content");
+console.log("After update : ");
+console.log(fs.readFileSync(filePath,'utf-8'));
+
+// D-delete
+fs.unlinkSync(filePath);
---
+++ module2/introTonodo/testModule.js
@@ -0,0 +1,8 @@
+let calc = require("./calculator.js");
+// console.log(calc);
+
+console.log(calc.addition(2,3));
+
+console.log(calc.multiply(2,3));
+
+console.log(calc.division(2,3));
