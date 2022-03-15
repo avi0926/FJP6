7a22fee8dce855abc89e70ed1fdb3fff19f02208
avi0926 <abhinandanmaurya230@gmail.com>
2022-03-16 00:14:08 +0530

    done

--- module2/introTonodo/question/q3.js
+++ module2/introTonodo/question/q3.js
@@ -1,1 +1,15 @@
 //copy index.html file from module1 to a new folder inside module2 having name html.
+let fs = require("fs");
+
+let path = require("path");
+
+//..means step back
+
+let srcpath = path.join(__dirname,"..","..","..","module4","index.html");
+console.log(srcpath);
+
+let destpath = path.join(__dirname,"..","..","index.html");
+console.log(destpath);
+
+
+fs.copyFileSync(srcpath,destpath);
--- module2/introTonodo/question/q5.js
+++ module2/introTonodo/question/q5.js
@@ -1,1 +1,13 @@
 //move a file
+
+let fs = require("fs");
+
+let path = require("path");
+
+let srcpath = path.join(__dirname,"file1.txt");
+
+let destpath = path.join(__dirname,"newFolder","file1.txt");
+
+fs.copyFileSync(srcpath,destpath);
+
+fs.unlinkSync(srcpath);
